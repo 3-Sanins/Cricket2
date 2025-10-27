c96837039ce11c1f2f2281f458f6ed6f96c16471
3-Sanins <sanins121421@gmail.com>
2025-10-27 16:33:49 +0530

    Update my_data.js

--- my_data.js
+++ my_data.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
   const firebaseConfig = {
-    apiKey: "AIzaSyDBhHt1wbY-gwlmUYkLSWblqgs8sptCWps",
-    authDomain: "cricket-f3711.firebaseapp.com",
-    databaseURL: "https://cricket-f3711-default-rtdb.firebaseio.com",
-    projectId: "cricket-f3711",
-    storageBucket: "cricket-f3711.firebasestorage.app",
-    messagingSenderId: "84308127741",
-    appId: "1:84308127741:web:9d7cc9843558d4d053b394"
+    apiKey: "AIzaSyClRQbU3N7F2F9Pp6BYirjcQxZEyVuxcXo",
+    authDomain: "cric-283bd.firebaseapp.com",
+    databaseURL: "https://cric-283bd-default-rtdb.firebaseio.com",
+    projectId: "cric-283bd",
+    storageBucket: "cric-283bd.firebasestorage.app",
+    messagingSenderId: "509305000521",
+    appId: "1:509305000521:web:2c16c4f7d2e85f98476598"
   };
-
   if (typeof firebase !== 'undefined') {
     firebase.initializeApp(firebaseConfig);
     const database = firebase.database();
